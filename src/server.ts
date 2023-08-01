80ef3d6452bd169edb50c1f8879df12987cc1c34
yurilinc-dev <yurilinc.tech@gmail.com>
2023-08-01 18:06:44 -0300

    insert, create, select connection with database

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
-import fastify from 'fastify'
-import { knex } from './database'
+import fastify from 'fastify';
+import crypto from 'node:crypto';
+import { knex } from './database';
+
+
 const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
 // http://localhost:3333/hello
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+  .where('amount', 1000)
+  .select('*')
 
-  return tables
-})
+  return transaction
+});
+
 app.listen({
-    port: 3333,
-  })
-  .then(() => {
-    const wordstring = 'HTTP Server.\nStatus: Running'
-    console.log(wordstring + "\n\nTo pause the application, press 'Ctrl + C'.")
-  })
+  port: 3333
+}).then(() => {
+  const wordstring = 'HTTP Server.\nStatus: Running';
+  console.log(wordstring + "\n\nTo pause the application, press 'Ctrl + C'.");
+});
